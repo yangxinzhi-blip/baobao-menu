71699f36d986dd692ec147b0af9830c9efe1e955
yangxinzhi-blip <3059588781@qq.com>
2026-07-12 19:36:20 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,5 @@
+﻿const CACHE = 'baobao-menu-v1';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => { if(event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : new Response('', {status:503})))); });
